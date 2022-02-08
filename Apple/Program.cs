bdd4e95ddebf7e934e7f019c8d162b67628b41b0
Jojo Joestar <USER_EMAIL>
2022-02-08 15:55:46 +0100

    afdasd

--- Apple/Program.cs
+++ Apple/Program.cs
@@ -7,9 +7,39 @@ namespace Apple
         static void Main(string[] args)
         {
             string a = "apple";
-            a = "2022.02.08";
-            Console.WriteLine(a);
+            int b = 42;
+            do
+            {
+
+            } while (a is "apple")
+            if (a is "apple")
+            {
+                Console.WriteLine(a);
+                a = "2022.02.08";
+
+            }
+            else {
+                
+                Console.WriteLine(Apple(Convert.ToInt32(Console.ReadLine()), a));
+            }
+
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static int Apple(int b, string a)
+        {
+            int j=10000;
+            if(a is "2022.02.08")
+            {
+                j = 42;
+                return j;
+            }
+            else
+            {
+                return j;
+            }
+            
         }
     }
 }
